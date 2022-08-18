10c9ef7ea246e27243f0f0b728b2643cc46f7e9c
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-18 18:01:10 +0530

    fix: Prism is not defined

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ import { setup } from "twind";
 import { virtualSheet } from "twind/sheets";
 import { config } from "utils/twind.ts";
 
+import "https://esm.sh/prismjs@1.25.0?pin=v55"
 import "https://esm.sh/prismjs@1.25.0/components/prism-jsx.js?no-check&pin=v55";
 import "https://esm.sh/prismjs@1.25.0/components/prism-typescript.js?no-check&pin=v55";
 import "https://esm.sh/prismjs@1.25.0/components/prism-tsx.js?no-check&pin=v55";
