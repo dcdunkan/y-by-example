11ea6705b65eff884a8fe2cccc944d07afdec38a
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-08-19 14:48:00 +0530

    fix: custom order for contents

--- utils/contents.ts
+++ utils/contents.ts
@@ -1,10 +1,3 @@
-const CONTENTS: string[] = [];
-
-for await (const { name } of Deno.readDir("contents")) {
-  CONTENTS.push(name.split(".")[0]);
-}
-
-const index = CONTENTS.indexOf("contents");
-if (index > -1) CONTENTS.splice(index, 1);
-
-export { CONTENTS };
+export const CONTENTS = [
+  "hello-world"
+];
