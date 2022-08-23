359cf66611bb638bcc90981c57d790099525d102
Roj <ez@roj.im>
2022-08-23 21:14:45 +0300

    mh

--- islands/RunButton.tsx
+++ islands/RunButton.tsx
@@ -8,20 +8,33 @@ export default function RunButton({ id }: { id: string }) {
     Object.fromEntries(
       document.cookie.split(";").map((v) => v).map((v) => v.split("=")),
     ).bot_token;
-  let ip = false;
+  // deno-lint-ignore no-explicit-any
+  let bot: any;
+  let busy = false;
+  let running = false;
 
   async function run() {
-    if (ip) {
-      return false;
+    if (busy) {
+      return;
     }
-    ip = true;
-    const url = new URL(`/static/${id}.ts`, location.href);
-    const { getBot } = await import(
-      `https://bundle.deno.dev/${url.toString()}`
-    );
-    const bot = getBot(botToken());
-    bot.start({ onStart: () => console.log(1) });
-    ip = false;
+    busy = true;
+    if (running) {
+      const url = new URL(`/static/${id}.ts`, location.href);
+      const { getBot } = await import(
+        `https://bundle.deno.dev/${url.toString()}`
+      );
+      const bot = getBot(botToken());
+      bot.start({
+        drop_pending_updates: true,
+        onStart: () => {
+          busy = false;
+          running = true;
+        },
+      });
+    } else {
+      bot.stop();
+      busy = false;
+    }
   }
 
   useEffect(() => {
