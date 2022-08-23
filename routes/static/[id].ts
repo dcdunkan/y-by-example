059d6a8ca16b4e1b52f3bc03ccb0147e014af319
Roj <ez@roj.im>
2022-08-23 20:51:12 +0300

    mh

--- routes/static/[id].ts
+++ routes/static/[id].ts
@@ -2,7 +2,10 @@ import { Handler } from "$fresh/server.ts";
 
 export const handler: Handler = async (req, ctx) => {
   return new Response(
-    await Deno.readTextFile(`./contents/${ctx.params.id}`),
+    (await Deno.readTextFile(`./contents/${ctx.params.id}`)).replace(
+      /(\n)(bot\.start\(\);)/,
+      "$1// $2",
+    ) + "\nexport { bot };\n",
     {
       headers: { "content-type": "application/typescript" },
     },
