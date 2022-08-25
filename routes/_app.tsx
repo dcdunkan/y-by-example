2bf1dee9e367f8616a7eaded8444bba2c1b10ad5
Roj <ez@roj.im>
2022-08-25 11:27:55 +0300

    try fixing the font

---
+++ routes/_app.tsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+/** @jsxFrag Fragment */
+import { Fragment, h } from "preact";
+import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        />
+      </Head>
+      <Component />
+    </>
+  );
+}
