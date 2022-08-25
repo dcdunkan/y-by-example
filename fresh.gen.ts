587bf605d4306b58611026577e8404e65b240110
Roj <ez@roj.im>
2022-08-25 11:32:20 +0300

    bring it to an end

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,18 +3,20 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[id].tsx";
-import * as $1 from "./routes/gfm.css.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/static/[id].ts";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/gfm.css.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/static/[id].ts";
 import * as $$0 from "./islands/BotTokenInput.tsx";
 import * as $$1 from "./islands/RunButton.tsx";
 
 const manifest = {
   routes: {
     "./routes/[id].tsx": $0,
-    "./routes/gfm.css.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/static/[id].ts": $3,
+    "./routes/_app.tsx": $1,
+    "./routes/gfm.css.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/static/[id].ts": $4,
   },
   islands: {
     "./islands/BotTokenInput.tsx": $$0,
