15fdea64cb4c261760e8081dd7d51265973f5ad4
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-09-15 18:37:50 +0530

    Fix styling

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,19 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  darkMode: "class",
+  mode: "silent",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Epilogue", "sans-serif"],
+      },
+      colors: {
+        grammy: {
+          400: "#38BDF8",
+          500: "#0EA5E9",
+        },
+      },
+    },
+  },
 } as Options;
